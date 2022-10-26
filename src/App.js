76891f70305c2c8185169388b861fd6dd27d5e75
JosephW143 <josephwright1436@gmail.com>
2022-10-25 22:17:48 -0400

    tests

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
-import ContactForm from './components/ContactForm';
+import ContactForm from './components/Contact';
 
 function App() {
-  const [categories] = useState([ 
+  const [categories] = useState([
     {
       name: 'commercial',
       description: 'Photos of grocery stores, food trucks, and other commercial projects',
@@ -35,12 +35,11 @@ function App() {
             <About></About>
           </>
         ) : (
-            <ContactForm></ContactForm>
-          )}
+          <ContactForm></ContactForm>
+        )}
       </main>
     </div>
   );
 }
 
 export default App;
-
